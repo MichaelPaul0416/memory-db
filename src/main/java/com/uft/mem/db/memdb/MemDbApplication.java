f3bbd3492343ca5b6103a58ae3bf96b6305c585e
MichaelPaul9416 <18357903752m0@sina.cn>
2019-06-21 22:29:13 +0800

    init

---
+++ src/main/java/com/uft/mem/db/memdb/MemDbApplication.java
@@ -0,0 +1,13 @@
+package com.uft.mem.db.memdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemDbApplication.class, args);
+    }
+
+}
